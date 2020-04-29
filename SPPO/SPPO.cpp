ecf3cb8bcb0aa728cda55fff2d43a131fe25ef92
CreateTAT <64464551+CreateTAT@users.noreply.github.com>
2020-04-29 18:42:56 +0300

    Update SPPO.cpp

--- SPPO/SPPO.cpp
+++ SPPO/SPPO.cpp
@@ -1,48 +1,99 @@
 ﻿#include <iostream>
-#include <cmath>
+#include "Windows.h"
+
 
 using namespace std;
 
-double f1(double a) {
-    double v;
-    if (a < 0)
-        v = abs(a);
-    else
-        v = exp(a + 2);
-    return v;
+
+struct element {
+	int value;
+	element* pointer;
+	element() {
+		value = 0;
+		pointer = NULL;
+	}
+};
+
+void InputList(element*& s, int n)
+{
+	element* p = NULL;
+	int i = 0;
+	cout << "Введите элементы списка ";
+	while (i < n) {
+		element* p1 = new element();
+		cin >> p1->value;
+		if (s == NULL) {
+			s = p1;
+			p = p1;
+		}
+		else {
+			p->pointer = p1;
+			p = p1;
+		}
+		i++;
+	}
 }
 
-double f2(double& m) {
-    m = f1(m);
-    return m;
+void OutputList(element* l) {
+	cout << "Список: ";
+	while (l != NULL) {
+		cout << l->value << " ";
+		l = l->pointer;
+	}
+	cout << endl;
 }
 
-int main() {
-    setlocale(LC_ALL, "rus");
+void DeleteFromList(element*& first, int n) 
+{
+	element* l = first, *t, * q, *i = first;
+	element* p = NULL;
+	while (l != NULL) {
+		for (t = q = i->pointer; q != NULL; ) {
+			if (l->value == i->value) {
+				q = l->pointer;
+				if (t == l)
+					t = i->pointer = l->pointer;
+				else
+					t->pointer = l->pointer;
+				delete l;
+				l = q;
+				continue;
+			}
+			t = l;
+			l = l->pointer;
+		}
+		i = i->pointer;
+	}
+}
 
-    double x, a, y, z;
+ void AddToList(element* first, int value) 
+{
+	element* i = new element;
+	if (i != NULL) {
+		i->value = value;
+		i->pointer = first;
+		first = i;
+	}
+	return(i != NULL);
+}
 
-    cout « "Введите первое значение ";
-    cin » x;
-    cout « "Введите количество ";
-    cin » y;
-    cout « "Введите шаг ";
-    cin » z;
 
-    a = x;
+int main() 
+{
+	setlocale(LC_ALL, "rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 
-    for (int i = -10; i <= y; i++, x += z) {
-        cout « "Результат " « f1(x) « " к значению " « x;
-        cout « endl;
-    }
-
-    cout « endl;
-
-    for (int i = -0.0003; i <= y; i++, a += z) {
-        cout « "Результат " « f2(a) « " к значению " « a;
-        cout « endl;
-    }
-
-    system("pause");
-    return 0;
+	element* first = NULL;
+	int n;
+     cout << "Введите длину списка ";
+	cin >> n;
+	InputList(first, n);
+	OutputList(first);
+	DeleteFromList(first, n);
+	AddToList (first,0);
+	OutputList(first);
+	
+     system ("pause");
+	return 0;
 }
